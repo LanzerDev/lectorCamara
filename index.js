afc82115b0f53f2d19daa7b6d6c30ca12f5ec9fd
lanzerdev <lanzerdev20@gmail.com>
2025-01-06 15:33:41 -0700

    Enhance barcode scanner UI and functionality: update HTML structure, add capture button, and improve event handling in JavaScript

--- index.js
+++ index.js
@@ -2,35 +2,46 @@
 
 const labelResult = document.querySelector("#result");
 const btnStart = document.querySelector("#btnStart");
+const btnScan = document.querySelector("#btnScan");
+btnScan.style = "display: none";
 
-Quagga.init({
-    inputStream: {
-        type: "LiveStream",
-        target: document.querySelector("#camera"), // Elemento donde se mostrará la cámara
-        constraints: {
-            width: 500,
-            height: 300,
-            facingMode: "environment" // Usa la cámara trasera del móvil
+function startScanner() {
+    Quagga.init({
+        inputStream: {
+            type: "LiveStream",
+            target: document.querySelector("#camera"), // Elemento donde se mostrará la cámara
+            constraints: {
+                width: 500,
+                height: 300,
+                facingMode: "environment" // Usa la cámara trasera del móvil
+            }
+        },
+        decoder: {
+            readers: ["code_128_reader", "ean_reader", "upc_reader"]
         }
-    },
-    decoder: {
-        readers: ["code_128_reader", "ean_reader", "upc_reader"]
-    }
-}, function(err) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    // console.log("Quagga inicializado correctamente");
-    // Quagga.start();
-});
+    }, function(err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+}
 
 btnStart.addEventListener("click", function() {
+    startScanner();
+    btnStart.style = "display: none";
+    btnScan.style = "display: block";
+    labelResult.textContent = "Cuando este listo, de click en capturar";
+});
+
+btnScan.addEventListener("click", function() {
     Quagga.start();
 });
 
 // Manejar el resultado del escaneo
 Quagga.onDetected(function(result) {
+    btnStart.style = "display: block";
+    btnScan.style = "display: none";
     console.log("Código detectado:", result.codeResult.code);
     labelResult.textContent = "Código detectado:" + result.codeResult.code;
     Quagga.stop();
